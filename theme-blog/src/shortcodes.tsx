c5af57c68356f7bcc2593a72f6ef9e3cf2592380
Fiona Hopkins <fiona@draughtleaders.com>
2019-09-16 22:16:36 -0400

    Adds Box and Flex as shortcodes

--- theme-blog/src/shortcodes.tsx
+++ theme-blog/src/shortcodes.tsx
@@ -1,4 +1,12 @@
+/** @jsx jsx */
+
+import { jsx, Box as BaseBox, Flex as BaseFlex } from 'theme-ui';
 import YouTube from '../src/components/YouTube';
+
+// Wrappers around Box and Flex that let us pass sx props through from MDX.
+// https://github.com/system-ui/theme-ui/issues/178
+const Box = props => <BaseBox {...props} />;
+const Flex = props => <BaseFlex {...props} />;
 
 /**
  * Define all shortcodes needed in blog posts here.
@@ -6,6 +14,6 @@ import YouTube from '../src/components/YouTube';
  * Shortcodes defined here will be available when the RSS feed generation is
  * run.
  */
-const MDX_COMPONENTS = { YouTube };
+const MDX_COMPONENTS = { YouTube, Box, Flex };
 
 export default MDX_COMPONENTS;
